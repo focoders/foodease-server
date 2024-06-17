4bc450a23204c0eec3c81a28d445e215f4d18325
clarkkkkk <juanfarrel0404@gmail.com>
2024-06-18 02:20:13 +0700

    feat: add schema to unified response and transfer object

--- src/api/v1/user/user.schema.ts
+++ src/api/v1/user/user.schema.ts
@@ -1,3 +1,5 @@
+import { IGetCustomerWithAddressResult } from "./user.queries";
+
 export type CustomerRegisterSchema = {
     email: string;
     first_name: string;
@@ -6,12 +8,24 @@ export type CustomerRegisterSchema = {
     confirm_password: string;
 }
 
+export type CustomerLoginSchema = {
+    email: string;
+    customer_password: string;
+}
 
-export function convertCustomerResponse(customer: any){
+export function convertCustomerResponse(customer: IGetCustomerWithAddressResult){
     return {
         email: customer.email,
         first_name: customer.first_name,
         last_name: customer.last_name,
-        active_address: null,
+        active_address: customer.address_id ? {
+            id: customer.address_id,
+            street: customer.address_street,
+            coordinates: customer.address_coordinates,
+            created_at: customer.address_created_at,
+            updated_at: customer.address_updated_at
+        } : null,
+        created_at: customer.created_at,
+        updated_at: customer.updated_at
     }
 }
