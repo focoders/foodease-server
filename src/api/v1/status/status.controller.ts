8e3dfcd73af678590f409a4403717b0b14a3454c
clarkkkkk <juanfarrel0404@gmail.com>
2024-06-17 16:09:42 +0700

    fix: utils directory

--- src/api/v1/status/status.controller.ts
+++ src/api/v1/status/status.controller.ts
@@ -1,26 +1,25 @@
 import { Request, Response } from "express";
 import pool from "@/database/pool";
-import { apiResponse } from "@/api/utils/response";
+import { apiResponse } from "@/utils/response";
 
 export default class StatusController {
-    static nodeServerStatusCheck (_:Request, res:Response){
-        return res.status(200).json(
-            apiResponse(null, true, 'Server status is normal and server is running')
-        )
-    }
-    
-    static async databaseStatusCheck (_:Request, res:Response){
-        try {   
-            await pool.query("SELECT NOW()");
-            return res.status(200).json(
-                apiResponse(null, true, 'Database is up and running')
-            )
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json(
-                apiResponse(null, false, "Database is down")
-            );
-        }
-    }
+  static nodeServerStatusCheck(_: Request, res: Response) {
+    return res
+      .status(200)
+      .json(
+        apiResponse(null, true, "Server status is normal and server is running")
+      );
+  }
 
+  static async databaseStatusCheck(_: Request, res: Response) {
+    try {
+      await pool.query("SELECT NOW()");
+      return res
+        .status(200)
+        .json(apiResponse(null, true, "Database is up and running"));
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(apiResponse(null, false, "Database is down"));
+    }
+  }
 }
--- src/api/v1/user/user.controller.ts
+++ src/api/v1/user/user.controller.ts
@@ -2,59 +2,64 @@ import { Request, Response } from "express";
 import { findUserByEmail, registerNewCustomer } from "./user.queries";
 import pool from "@/database/pool";
 import { ParamsDictionary } from "express-serve-static-core";
-import { apiResponse } from "@/api/utils/response";
+import { apiResponse } from "@/utils/response";
 import { CustomerRegisterSchema } from "./user.schema";
 
 export default class UserController {
-    static async findUser(req: Request, res: Response){
-        try {
-            const user = await findUserByEmail.run({
-                email: 'juanfarrel0404@gmail.com'
-            }, pool)
-            if(!user || user.length === 0){
-                return res.status(404).json(
-                    apiResponse(null, false, "User not found")
-                )
-            }
-    
-            return res.status(200).json(
-                apiResponse(user, true, "User found")
-            )
-            
-        } catch (error) {
-            console.error(error)
-            return res.status(500).json(
-                apiResponse(null, false, "Internal Server Error")
-            )
-        }
+  static async findUser(req: Request, res: Response) {
+    try {
+      const user = await findUserByEmail.run(
+        {
+          email: "juanfarrel0404@gmail.com",
+        },
+        pool
+      );
+      if (!user || user.length === 0) {
+        return res.status(404).json(apiResponse(null, false, "User not found"));
+      }
+
+      return res.status(200).json(apiResponse(user, true, "User found"));
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json(apiResponse(null, false, "Internal Server Error"));
     }
+  }
 
-    static async registerCustomer(req: Request<ParamsDictionary, any, CustomerRegisterSchema>, res: Response){
-        try {
-            const existedCustomer = await findUserByEmail.run({email: req.body.email}, pool);
-            if(existedCustomer.length > 0){
-                return res.status(400).json(
-                    apiResponse(null, false, "User already registered")
-                )
-            }
+  static async registerCustomer(
+    req: Request<ParamsDictionary, any, CustomerRegisterSchema>,
+    res: Response
+  ) {
+    try {
+      const existedCustomer = await findUserByEmail.run(
+        { email: req.body.email },
+        pool
+      );
+      if (existedCustomer.length > 0) {
+        return res
+          .status(400)
+          .json(apiResponse(null, false, "User already registered"));
+      }
 
-            const newCustomer = await registerNewCustomer.run({
-                customer: {
-                    email: req.body.email,
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    customer_password: req.body.customer_password
-                }
-            }, pool)
+      const newCustomer = await registerNewCustomer.run(
+        {
+          customer: {
+            email: req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            customer_password: req.body.customer_password,
+          },
+        },
+        pool
+      );
 
-            return res.json(newCustomer)
-            
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json(
-                apiResponse(null, false, "Internal Server Error")
-            )
-        }
-    }   
-
+      return res.json(newCustomer);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json(apiResponse(null, false, "Internal Server Error"));
+    }
+  }
 }
