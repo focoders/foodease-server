90afd9c833d23a948ab48596dff7e36e26479600
clarkkkkk <juanfarrel0404@gmail.com>
2024-06-18 23:26:01 +0700

    feat: add auth token verify services as middleware

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,3 +1,5 @@
+import { apiResponse } from "@/utils/response"
+import { NextFunction, Request, Response } from "express"
 import jwt from "jsonwebtoken"
 
 export function generateAuthToken(data: string){
@@ -6,4 +8,29 @@ export function generateAuthToken(data: string){
         throw new Error("JWT secret key is required")
     }
     return jwt.sign({data}, secret, { expiresIn: '1d'})
+}
+
+export function verifyAuthToken(req: Request, res: Response, next: NextFunction){
+    const secret_key = process.env.JWT_AUTH_SECRET_KEY
+    if(!secret_key){
+        throw new Error("Invalid secret key")
+    }
+
+    const authorization = req.headers.authorization
+    const type = authorization?.split(" ")[0]
+    if(authorization == undefined || authorization == null || type != "Bearer"){
+        throw new Error("Invalid authorization")
+    }
+    const token = authorization.split(" ")[1]
+    
+    try {
+        const decodedToken = jwt.verify(token, secret_key)
+        req.body.payload = decodedToken
+        return next();
+    } catch (error) {
+        console.error(error)
+        return res.status(401).json(
+            apiResponse(null, false, "Customer unauthorized")
+        )
+    }
 }
